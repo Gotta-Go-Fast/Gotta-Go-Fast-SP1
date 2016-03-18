e78ffe70f04a2802a9198be8ae8147a0d0b15e73
Oscar <oscar_pleininger@hotmail.com>
2016-03-18 14:28:54 +0100

    Added Tutorial game
    La till allt jag gjort

---
+++ MakeAGameTutorial2D/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public float jumppower = 20;
+	private Rigidbody rb;
+	private bool isOnGround = true;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+		rb.AddForce (movement * speed);
+
+		/*
+		if (!isOnGround && rb.velocity.y == 0) 
+		{
+			isOnGround = true;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space) && isOnGround) 
+		{
+			rb.AddForce (transform.up * jumppower);
+			isOnGround = false;
+		}
+		*/
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pickup")) 
+		{
+			other.gameObject.SetActive(false);
+		}
+	}
+}
---
+++ MakeAGameTutorial2D/Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	public int Xrotation;
+	public int Yrotation;
+	public int Zrotation;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (Xrotation, Yrotation, Zrotation) * Time.deltaTime);
+	}
+}
